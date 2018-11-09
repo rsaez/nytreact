ccac76a997598ef290aa7197e56b099b733ee0e1
Rodolfo Saez <rolo928@hotmail.com>
2018-11-09 00:09:47 -0500

    added mongoose connection

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
+// Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the MONGODB_URI
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact",
+  {
+    useMongoClient: true
+  }
+);
 
 // Define API routes here
 
